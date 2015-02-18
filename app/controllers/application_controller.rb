8309d2f2c153d2ec5c8e68fe7a6aefb99cfe3a3c
Your Name <you@example.com>
2015-02-17 22:00:51 -0800

    added name field to users model, added fieleds in the devise views, and updated the applcation controller to pass them in via strong paarms in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  private
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) << [:name]
+    devise_parameter_sanitizer.for(:account_update) << [:name]
+  end
+
 end
